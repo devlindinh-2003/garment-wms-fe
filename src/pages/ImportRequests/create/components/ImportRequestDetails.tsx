36e79db86220212584a603290ffc8294008f2ed3
baotam546 <donguyenbaotam546@gmail.com>
2024-10-11 00:00:59 +0700

    resolve conflict [TamDNB]

--- src/pages/ImportRequests/create/components/ImportRequestDetails.tsx
+++ src/pages/ImportRequests/create/components/ImportRequestDetails.tsx
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import type { ImportRequestDetails } from '@/types/ImportRequestType';
-import { useState } from 'react';
-import MaterialForm from './MaterialForm';
-// import DataTable from '@/components/common/EditableTable/DataTable';
-import { Button } from '@/components/ui/button';
-import EditMaterialForm from './EditMaterialForm';
-type Props = {};
-
-// Replace this with your actual initial data or import
-const initialDetails = [
-  { materialId: "1", materialName: "Material 1", SKU: "SKU1", UOM: "pcs", plannedQuantity: 10, actualQuantity: 10 },
-  { materialId: "2", materialName: "Material 2", SKU: "SKU2", UOM: "pcs",plannedQuantity: 10, actualQuantity: 10 },
-  // Add more materials as needed
-];
-const ImportRequestDetails = (props: Props) => {
-  const [isDialogCreateOpen, setIsDialogCreateOpen] = useState<boolean>(false);
-  const [selectedMaterial, setSelectedMaterial] = useState<any>(null);
-  const [details, setDetails] = useState(initialDetails);
-  const [isDialogEditOpen, setIsDialogEditOpen] = useState<boolean>(false);
-
-  // const onEdit = useCallback((material: ImportRequestDetails) => {
-  //   setSelectedMaterial(material);
-  //   setIsDialogEditOpen(true);
-  // }, []);
-  // const onDelete = useCallback((material: ImportRequestDetails) => {
-  //   const updatedDetails = details.filter(item => item.materialId !== material.materialId);
-  //   setDetails(updatedDetails);
-  // }, [details]);
-  // const columns = useMemo(() => getMaterialColumns({ onEdit, onDelete }), []);
-=======
 import React, { useEffect, useMemo, useState } from 'react';
 import { getMaterialColumns } from './MaterialColumns';
 import DataTable from '@/components/common/EditableTable/DataTable';
@@ -82,54 +51,12 @@ const ImportRequestDetails = ({
     setPoDeliverydetails(details);
   };
   const columns = useMemo(() => getMaterialColumns({}), []);
->>>>>>> ec216b7d54be3e4d070f00e859e78c2a8ea759ac
   return (
     <div className="px-4">
       <div className="flex flex-col gap-4">
         <div className="font-primary font-bold text-xl mb-4">Import Request Details</div>
       </div>
 
-<<<<<<< HEAD
-      <div className="flex justify-between mb-4">
-        <div />
-        {/* Add Button to Open the Dialog */}
-        <Button onClick={() => setIsDialogCreateOpen(true)}>Add Material</Button>
-      </div>
-        <div className="flex justify-between">
-          <div />
-          <div className="flex-nowrap">
-          <MaterialForm
-        isOpen={isDialogCreateOpen}
-        onOpenChange={(value) => {
-          setIsDialogCreateOpen(value);
-        }}
-        details={details} 
-        setDetails={setDetails} 
-      />
-          </div>
-        </div>
-
-        <div className="flex justify-between">
-          <div />
-          <div className="flex-nowrap">
-          <EditMaterialForm
-          material={selectedMaterial}
-        isOpen={isDialogEditOpen}
-        onOpenChange={(value) => {
-          setIsDialogEditOpen(value);
-          if (!value) {
-            setSelectedMaterial(null);
-          }
-        }}
-        details={details} 
-        setDetails={setDetails} 
-      />
-          </div>
-        </div>
-      
-        {/* <DataTable data={details} columns={columns}/> */}
-
-=======
       <div className="flex justify-end mb-4">
         {isEditDetail && (
           <Button className="mr-4" onClick={handleSave}>
@@ -142,7 +69,6 @@ const ImportRequestDetails = ({
       {details && (
         <DataTable data={details} columns={columns} isEdit={isEditDetail} setDetails={setDetails} />
       )}
->>>>>>> ec216b7d54be3e4d070f00e859e78c2a8ea759ac
     </div>
   );
 };
